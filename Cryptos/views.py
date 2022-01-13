c4a48375de9d53144f575eefdd849e2a8e7ec65f
Dinesh <dineshkumar.software@gmail.com>
2022-01-13 14:39:54 +0530

    partially working index

--- Cryptos/views.py
+++ Cryptos/views.py
@@ -3,4 +3,4 @@ import requests
 
 def IndexCrypto(request):
     data = requests.request("GET", "http://127.0.0.1:8000/static/Coins.json")
-    return render(request, "IndexCrypto.html", {'Data':data})
+    return render(request, "IndexCrypto.html", {'Data':data.json()["data"]["coins"]})
