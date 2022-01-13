a2f8c03a2d2cb0628d6ccec72766d53839184dfb
Dinesh <dineshkumar.software@gmail.com>
2022-01-13 13:05:02 +0530

    navbar

--- Cryptos/views.py
+++ Cryptos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import requests
 
 def IndexCrypto(request):
-    return render(request, "IndexCrypto.html")
+    data = requests.request("GET", "http://127.0.0.1:8000/static/Coins.json")
+    return render(request, "IndexCrypto.html", {'Data':data})
--- StockScreener/urls.py
+++ StockScreener/urls.py
@@ -22,5 +22,5 @@ from Cryptos.views import IndexCrypto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("Cryptos", IndexCrypto)
+    path("Cryptos", IndexCrypto, name = "CryptoIndex")
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
