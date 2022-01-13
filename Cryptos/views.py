83000a941b1516dd651fd457b8e122f67ee13505
Dinesh <dineshkumar.software@gmail.com>
2022-01-13 19:32:49 +0530

    removed sparkline

--- Cryptos/views.py
+++ Cryptos/views.py
@@ -3,4 +3,5 @@ import requests
 
 def IndexCrypto(request):
     data = requests.request("GET", "http://127.0.0.1:8000/static/Coins.json")
-    return render(request, "IndexCrypto.html", {'Data':data.json()["data"]["coins"]})
+    data = data.json()["data"]["coins"]
+    return render(request, "IndexCrypto.html", {'Data': data})
