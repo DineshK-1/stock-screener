5fd6a182409bedd05987f17ea655df2012f34e9d
Dinesh <dineshkumar.software@gmail.com>
2022-01-14 12:32:14 +0530

    added ohlc

--- Cryptos/views.py
+++ Cryptos/views.py
@@ -26,4 +26,22 @@ def DetailedCrypto(request, uuid):
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    return render(request, "DetailedCrypto.html", {'Data': response.json()["data"]["coin"]})
+    ohlcurl = "https://coinranking1.p.rapidapi.com/coin/"+uuid+"/ohlc"
+    ohlc = requests.request("GET", ohlcurl, headers=headers, params={"referenceCurrencyUuid":"yhjMzLPhuIDl","interval":"day", "limit":"1"})
+    ohlc = ohlc.json()
+
+    ohlc = ohlc["data"]["ohlc"][0]
+
+    return render(request, "DetailedCrypto.html", {'Data': response.json()["data"]["coin"], "OHLCData": ohlc})
+
+def MarketsCrypto(request, uuid):
+    url = "https://coinranking1.p.rapidapi.com/coin/"+uuid+"/markets"
+
+    querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl","limit":"50","offset":"0","orderBy":"24hVolume","orderDirection":"desc"}
+
+    headers = {
+    'x-rapidapi-host': "coinranking1.p.rapidapi.com",
+    'x-rapidapi-key': "1308a3e81fmshc5c7805a1477572p1ac776jsn696bc69cace6"
+    }
+    marketresponse = requests.request("GET", url, headers=headers, params=querystring)
+    return render(request, "DetailedCrypto.html", {'MarketData': marketresponse.json()["data"]})
