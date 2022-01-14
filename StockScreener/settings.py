f9268d40843bc7aae0f10b79ea1172af5519b160
Dinesh <dineshkumar.software@gmail.com>
2022-01-14 13:39:14 +0530

    added hosts and middleware

--- StockScreener/settings.py
+++ StockScreener/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q9a)&d=4m!o57z@!1&9am3n%4+xwyt!_@u$!$*+v$ftnv=wg9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "crypto-apes.herokuapp.com"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
