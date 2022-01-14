61b627f3479a04a3ad1f1165050eda2e46ce6f70
Dinesh <dineshkumar.software@gmail.com>
2022-01-14 11:27:53 +0530

    Api calls

--- Cryptos/views.py
+++ Cryptos/views.py
@@ -3,10 +3,27 @@ import requests
 import json
 
 def IndexCrypto(request):
-    data = requests.request("GET", "http://127.0.0.1:8000/static/Coins.json")
-    data = data.json()["data"]
-    return render(request, "IndexCrypto.html", {'Data': data})
+    url = "https://coinranking1.p.rapidapi.com/coins"
+
+    querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl","timePeriod":"24h","tiers":"1","orderBy":"marketCap","orderDirection":"desc","limit":"50","offset":"0"}
+
+    headers = {
+    'x-rapidapi-host': "coinranking1.p.rapidapi.com",
+    'x-rapidapi-key': "1308a3e81fmshc5c7805a1477572p1ac776jsn696bc69cace6"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return render(request, "IndexCrypto.html", {'Data': response.json()["data"]})
 
 def DetailedCrypto(request, uuid):
-    response = requests.request("GET", "http://127.0.0.1:8000/static/CoinCall.json")
+    url = "https://coinranking1.p.rapidapi.com/coin/"+uuid
+
+    querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl","timePeriod":"24h"}
+
+    headers = {
+    'x-rapidapi-host': "coinranking1.p.rapidapi.com",
+    'x-rapidapi-key': "1308a3e81fmshc5c7805a1477572p1ac776jsn696bc69cace6"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
     return render(request, "DetailedCrypto.html", {'Data': response.json()["data"]["coin"]})
